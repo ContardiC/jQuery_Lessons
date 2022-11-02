7490ec79b8ceeaa6c072590f145775d2d5eb7a39
Carlo Contardi <c.contardi@icloud.com>
2022-11-02 13:32:29 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// versione completa
+$(document).ready(function(){
+    console.log("Il documento è pronto");
+});
+// versione shorthand
+
+$(function(){
+    console.log("Il documento è pronto");
+});
